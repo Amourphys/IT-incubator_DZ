573702c9c005dc4ab5b530f5681d87f518b7b64b
Jurij Zakharov <amourphys@gmail.com>
2022-02-07 12:54:27 +0200

    Change structure of files and folders, complete 3task about input

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Pages.module.css'
 
 const Junior = () => {
     return (
         <div>
-            <h1>Junior Page</h1>
+            <h1 className={s.h1}>Junior Page</h1>
         </div>
     );
 };
--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Pages.module.css'
 
 const JuniorPlus = () => {
     return (
         <div>
-            <h1>Junior Plus Page</h1>
+            <h1  className={s.h1}>Junior Plus Page</h1>
         </div>
     );
 };
