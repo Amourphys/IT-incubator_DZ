6a85fa5e6c09da0e80916d0d21bcd307e3aa3a99
Jurij Zakharov <amourphys@gmail.com>
2022-02-19 16:27:27 +0200

    Change structure of files and folders, complete 3task about input

---
+++ src/p2-homeworks/h6/HW6.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {restoreState, saveState} from './localStorage/localStorage'
+import s from "./HW6.module.css";
+
+function HW6() {
+    const [value, setValue] = useState<string>('')
+
+    const save = () => {
+        saveState<string>('editable-span-value', value)
+    }
+    const restore = () => {
+         setValue(restoreState<string>('editable-span-value', ''))
+    }
+
+    return (
+        <div>
+            <hr/>
+            <h2 className={s.hwtitle}>homeworks 6</h2>
+
+            {/*should work (должно работать)*/}
+            <div className={s.wrapper}>
+                <div className={s.span}>
+                    <SuperEditableSpan
+                        value={value}
+                        onChangeText={setValue}
+                        spanProps={{children: value ? undefined : 'enter text...'}}
+                    />
+                </div>
+                <SuperButton onClick={save}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
+
+                <hr/>
+                {/*для личного творчества, могу проверить*/}
+                {/*<AlternativeSuperEditableSpan/>*/}
+                <hr/>
+            </div>
+
+        </div>
+    )
+}
+
+export default HW6
